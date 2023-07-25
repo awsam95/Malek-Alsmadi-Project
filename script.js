068aaadc904935372d4c25ec86b92a838b66162d
Awsam Barahmeh <awsam.haitham@outlook.com>
2023-07-26 01:21:49 +0300

    Finishing up section two

--- script.js
+++ script.js
@@ -30,3 +30,34 @@ function toggleHeaderText() {
 
 // Call the toggleHeaderText function every 6 seconds
 setInterval(toggleHeaderText, 5000);
+
+// JavaScript to handle slider navigation
+const sliderWrapper = document.querySelector('.slider-wrapper');
+const prevButton = document.querySelector('#prev-button');
+const nextButton = document.querySelector('#next-button');
+const cardWidth = 400 + 20; // Width of each card including the margin (showing 2 cards at a time)
+const totalCards = sliderWrapper.children.length;
+const maxIndex = Math.ceil(totalCards / 2) - 1; // Maximum index for the last visible card set
+
+let currentIndex = 0; // Current index of the first visible card
+
+prevButton.addEventListener('click', () => {
+  currentIndex = Math.max(currentIndex - 1, 0); // Show two cards at a time
+  updateSliderPosition();
+});
+
+nextButton.addEventListener('click', () => {
+  currentIndex = Math.min(currentIndex + 1, maxIndex); // Show two cards at a time
+  updateSliderPosition();
+});
+
+function updateSliderPosition() {
+  const translateXValue = -currentIndex * cardWidth;
+  sliderWrapper.style.transform = `translateX(${translateXValue}px)`;
+}
+
+
+
+
+
+
